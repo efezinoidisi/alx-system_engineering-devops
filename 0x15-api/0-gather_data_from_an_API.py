ad2947332ce09e07c88f40b6a2c02c118c41fb11
efezino idisi <enduranceidisi13@gmail.com>
2023-04-24 19:54:14 +0100

    fix: checker error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python3
-"""
-This module contains a function that gets data from a REST API:
-'https://jsonplaceholder.typicode.com/' and displays the details about the employee name and completed tasks
-
-
-it accepts an integer as a parameter, which is the employee ID
-FORMAT:
-First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-EMPLOYEE_NAME: name of the employee
-NUMBER_OF_DONE_TASKS: number of completed tasks
-TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
-Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
-"""
+"""This module contains a function that gets data from a REST API"""
 import requests
 from sys import argv
 _, id = argv
 
 
 def main():
-    """get employee name and information about their todo list given the
-    employee id
-    First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-    EMPLOYEE_NAME: name of the employee
-    NUMBER_OF_DONE_TASKS: number of completed tasks
-    TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
-    Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
-    """
+    """get and display information about an employee's todo list"""
     url = "https://jsonplaceholder.typicode.com/"
     todos = requests.get("{}users/{}/todos".format(url, id)).json()
     users = requests.get("{}users".format(url)).json()
