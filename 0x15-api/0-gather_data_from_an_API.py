a0838ae9010a2c95153f80b6982d949823bf4055
efezino idisi <enduranceidisi13@gmail.com>
2023-04-24 18:33:39 +0100

    update documemtation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""
-This module contains a function that gets data from a REST API:
+"""This module contains a function that gets data from a REST API:
 'https://jsonplaceholder.typicode.com/'
 """
-from sys import argv
 import requests
+from sys import argv
 _, id = argv
 
+
 def main():
-    """
-    get employee name and information about their todo list given the employee id
-    """
+    """get employee name and information about their todo list given the
+    employee id"""
     url = "https://jsonplaceholder.typicode.com/"
     todos = requests.get("{}users/{}/todos".format(url, id)).json()
     users = requests.get("{}users".format(url)).json()
@@ -18,12 +17,14 @@ def main():
     for item in users:
         if item['id'] == int(id):
             user.update(item)
-    completed = filter(lambda x: x['completed'] == True, todos)
+    completed = filter(lambda x: x.get('completed'), todos)
     completed = [item for item in completed]
-    text = "Employee {} is done with tasks({}/{}):".format(user.get('name'), len(completed), len(todos))
+    text = "Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(completed), len(todos))
     print(text)
     for task in completed:
         print("\t {}".format(task.get('title')))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
